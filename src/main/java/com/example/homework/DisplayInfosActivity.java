6906d76d5b562f08f15aa7c224c08b03c8f7485c
Ouassimch <97995273+Ouassimch@users.noreply.github.com>
2023-11-02 00:06:35 +0100

    Add files via upload

---
+++ src/main/java/com/example/homework/DisplayInfosActivity.java
@@ -0,0 +1,44 @@
+package com.example.homework;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+
+public class DisplayInfosActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_infos);
+
+        // Receive the input values from the intent
+        String firstName = getIntent().getStringExtra("firstName");
+        String lastName = getIntent().getStringExtra("lastName");
+        String email = getIntent().getStringExtra("email");
+        String password = getIntent().getStringExtra("password");
+        String phoneNumber = getIntent().getStringExtra("phoneNumber");
+
+        // Find the TextView in content_second.xml and set the text
+        TextView textView = findViewById(R.id.textFullName);
+        textView.setText(" "+firstName + " " + lastName);
+        TextView textView2 = findViewById(R.id.textEmail);
+        textView2.setText(" "+email);
+        TextView textView3 = findViewById(R.id.textPhone);
+        textView3.setText(" "+phoneNumber);
+        Button returnButton = findViewById(R.id.returnButton);
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // When the "Register" button is clicked, navigate to ActivitySecond
+                Intent intent = new Intent(DisplayInfosActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
